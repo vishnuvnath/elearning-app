01fed588db56d681ced850a6309f03e149eee45b
vishnuvnath <vishnunathayr88@gmail.com>
2020-11-24 17:51:15 +0530

    teacher page started

--- elearning/learn/views.py
+++ elearning/learn/views.py
@@ -10,6 +10,8 @@ from bs4 import BeautifulSoup
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.models import User, auth
+from django.db.models import Q
+
 
 
 def index(request):
@@ -120,18 +122,20 @@ def login(request):
         except:
             messages.info(request,'Invalid credentials')
             return render(request,'login.html')
-        g = User.objects.get(username = uss)
-        c = Registration.objects.filter(user = g)
-        for i in c:
-            if i.User_role == 'admin':
-                return render(request, 'adminhome.html',{'c':c})
-            if i.User_role == 'teacher':
-                return render(request, 'teacherhome.html',{'c':c})
-            if i.User_role == 'student':
-                return render(request, 'studenthome.html',{'c':c})
-            else:
-                messages.info(request, 'Your account to the website is blocked')
-                return render(request,'login.html')
+        else:
+            g = User.objects.get(username = uss)
+            c = Registration.objects.filter(user = g)
+
+            for i in c:
+                if i.User_role == 'admin':
+                    return render(request, 'adminhome.html',{'c':c})
+                elif i.User_role == 'teacher':
+                    return render(request, 'teacherhome.html',{'c':c})
+                elif i.User_role == 'student':
+                    return render(request, 'studenthome.html',{'c':c})
+                else:
+                    messages.info(request, 'Your account to the website is blocked')
+                    return render(request,'login.html')
     else:
         return render(request,'login.html')
 
@@ -141,7 +145,8 @@ def login1(request):
         uss = request.method == 'uss'
 
 def adminhome(request):
-    return render(request, 'adminhome.html')
+    c = Registration.objects.filter(User_role = 'admin')
+    return render(request, 'adminhome.html', {'c': c})
 
 
 def teacher_reg(request):
@@ -268,15 +273,21 @@ def view_blog(request):
     return render(request,'view_blog.html',{'dc':dc})
 
 def update_prof(request):
-    gtt = Registration.objects.filter(User_role = 'admin')
-    return render(request, 'update_prof.html',{'gtt':gtt})
+    bb1 = Registration.objects.get(User_role = 'admin')
+    return render(request, 'update_prof.html',{'bb1':bb1})
+
+
 
 def adm_prof(request):
     gtt = Registration.objects.filter(User_role = 'admin')
     return render(request, 'adm_prof.html',{'gtt':gtt})
 
+
+
 def del_admin(request, id):
-    Registration.objects.get(id = id).delete()
+    id = int(id)
+    mn = Registration.objects.get(id = id)
+    User.objects.get(username = mn.user).delete()
     messages.success(request, 'You have successfully resigned from administration')
     return render(request, 'index1.html')
 
@@ -284,11 +295,24 @@ def edit_admin(request):
     bb1 = Registration.objects.get(User_role = 'admin')
     return render(request, 'update_admin.html',{'bb1':bb1})
 
-
 def bnb(request):
     if request.method == 'POST':
         first = request.POST.get('first')
         last = request.POST.get('last')
+        em = request.POST.get('em')
+        psw = request.POST.get('psw')
+        dcd = Registration.objects.get(User_role = 'admin')
+        dcd.Email = em
+        dcd.Password = psw
+        dcd.First_name = first
+        dcd.Last_name = last
+        dcd.save()
+        gtt = Registration.objects.filter(User_role='admin')
+        messages.success(request, 'You have successfully updated your profile')
+        return render(request, 'adm_prof.html', {'gtt': gtt})
+    else:
+        return render(request, "admin_home.html")
+
 
 def blogs_admin(request):
     dm = Blogs.objects.all()
@@ -315,8 +339,48 @@ def blog_delete(request, id):
     dm = Blogs.objects.all()
     return render(request, 'blogs_admin.html', {'dm': dm})
 
+
 def upload_cert(request):
-    return render(request,'upload_cert.html')
+    ss = Registration.objects.filter(User_role = 'student')
+    bc = Enrollment.objects.all()
+    kk = []
+    kj = []
+    ks = []
+    ka = []
+    kb = []
+    kc = []
+    for i in ss:
+        for t in bc:
+           if i.Email == t.Student_email:
+               kk.append(i.First_name)
+               kj.append(i.Last_name)
+               ks.append(i.Email)
+               ka.append(t.Subject_name)
+               kb.append(t.Course_name)
+               kc.append(t.Teacher_email)
+    if request.method == 'POST':
+        stu_id = request.POST.get('stu_id')
+        gg = stu_id.split(";")
+        hu = gg[0]
+        tq = gg[1]
+        tp = gg[2]
+        wx = gg[3]
+        cert = request.FILES['cert']
+        fs = FileSystemStorage()
+        fs.save(cert.name,cert)
+        try:
+            cc = Enrollment.objects.get(Student_email = hu, Subject_name = tq, Course_name = tp, Teacher_email = wx)
+        except:
+            messages.success(request, 'Please delete old certificates of student')
+            return render(request, "admin_home.html")
+        cc.Certificate = cert
+        cc.save()
+        messages.success(request, 'Certificate uploaded successfully')
+        return render(request, "admin_home.html")
+    ms = zip(kk,kj,ks,ka,kb,kc)
+    for a,b,c,d,e,f in ms:
+        print(a,b,c,d,e,f)
+    return render(request,'upload_cert.html',{'ms':ms})
 
 def delete_cert(request):
     return render(request,'delete_cert.html')
@@ -349,7 +413,122 @@ def cont_ad(request):
     return render(request,'cont_ad.html')
 
 def logout(request):
-    return render(request,'logout')
+    auth.logout(request)
+    return render(request,'index1.html')
+
+def about_content(request):
+    amm = Registration.objects.get(User_role = 'admin')
+    c = Registration.objects.filter(User_role='admin')
+    if request.method == 'POST':
+        abbt = request.POST.get('abbt')
+        idd = request.POST.get('idd')
+        adc = Registration.objects.get(id = idd)
+        adc.About_website = abbt
+        adc.save()
+        messages.success(request, 'Content added successfully')
+        return render(request, 'adminhome.html',{'c':c})
+    return render(request,'about_content.html',{'amm':amm})
+
+
+
+
+
+
+
+def block(request):
+    t_reg = Registration.objects.filter(Q(User_role="teacher") | Q(User_role="teacher_blocked"))
+    s_reg = Registration.objects.filter(Q(User_role="student") | Q(User_role="student_blocked"))
+    return render(request,'block.html',{'t_reg':t_reg,'s_reg':s_reg})
+
+
+def blocks(request, id):
+    klk = Registration.objects.get(id=id)
+    klk.User_role = 'teacher_blocked'
+    klk.save()
+    t_reg = Registration.objects.filter(Q(User_role="teacher") | Q(User_role="teacher_blocked"))
+    s_reg = Registration.objects.filter(Q(User_role="student") | Q(User_role="student_blocked"))
+    return render(request, 'block.html', {'t_reg': t_reg, 's_reg': s_reg})
+
+
+def blocks1(request, id):
+    klk = Registration.objects.get(id=id)
+    klk.User_role = 'student_blocked'
+    klk.save()
+    t_reg = Registration.objects.filter(Q(User_role="teacher") | Q(User_role="teacher_blocked"))
+    s_reg = Registration.objects.filter(Q(User_role="student") | Q(User_role="student_blocked"))
+    return render(request, 'block.html', {'t_reg': t_reg, 's_reg': s_reg})
+
+
+def allows(request, id):
+    klk = Registration.objects.get(id=id)
+    klk.User_role = 'teacher'
+    klk.save()
+    t_reg = Registration.objects.filter(Q(User_role="teacher") | Q(User_role="teacher_blocked"))
+    s_reg = Registration.objects.filter(Q(User_role="student") | Q(User_role="student_blocked"))
+    return render(request, 'block.html', {'t_reg': t_reg, 's_reg': s_reg})
+
+
+def allows1(request, id):
+    klk = Registration.objects.get(id=id)
+    klk.User_role = 'student'
+    klk.save()
+    t_reg = Registration.objects.filter(Q(User_role="teacher") | Q(User_role="teacher_blocked"))
+    s_reg = Registration.objects.filter(Q(User_role="student") | Q(User_role="student_blocked"))
+    return render(request, 'block.html', {'t_reg': t_reg, 's_reg': s_reg})
+
+
+
+def teacher_prof(request):
+    return render(request,'teacher_prof.html')
+
+def student_progress(request):
+    return render(request,'student_progress.html')
+
+def change_password(request):
+    return render(request,'change_password.html')
+
+def subject(request):
+    return render(request,'subject.html')
+
+def chapter(request):
+    return render(request,'chapter.html')
+
+def chapter_content(request):
+    return render(request,'chapter_content.html')
+
+def Student_booked_details(request):
+    return render(request,'Student_booked_details.html')
+
+def teacher_messages(request):
+    return render(request,'teacher_messages.html')
+
+def attendence(request):
+    return render(request,'attendence.html')
+
+def scedule_test(request):
+    return render(request,'scedule_test.html')
+
+def delete_scedule_test(request):
+    return render(request,'delete_scedule_test.html')
+
+def results(request):
+    return render(request,'results.html')
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'index1.html')
+
+def teacher_prof(request):
+    bb1 = Registration.objects.get(User_role = 'teacher')
+    return render(request, 'teacher_prof.html',{'bb1':bb1})
+
+def tupdate_prof(request):
+    bb1 = Registration.objects.get(User_role = 'teacher')
+    return render(request, 'tupdate_prof.html',{'bb1':bb1})
+
+
+
+
 
 
 
